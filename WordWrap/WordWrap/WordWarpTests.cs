066cbc3af43200c9847b59c58b27e60b5df9e8b5
Hendrik Loesch <loeschhendrik@googlemail.com>
2013-05-27 20:59:54 +0200

    Added WordWrap Kata

---
+++ WordWrap/WordWrap/WordWarpTests.cs
@@ -0,0 +1,64 @@
+﻿namespace WordWrap
+{
+    using Shouldly;
+
+    using Xunit;
+
+    public class WordWarpTests
+    {
+        [Fact]
+        public void IndexOutOfRangeReturnsGivenWord()
+        {
+            string expected = "a";
+            WordWrapper.Wrap(expected, 1).ShouldBe(expected);
+        }
+
+        [Fact]
+        public void NullResultsInNull()
+        {
+            WordWrapper.Wrap(null, 7).ShouldBe(null);
+        }
+
+        [Fact]
+        public void SpaceAtMaxLengthIsReplacedByNewLine()
+        {
+            WordWrapper.Wrap("a b", 1).ShouldBe("a\nb");
+        }
+
+        [Fact]
+        public void SpaceBeforeMaxLengthIsReplacedByNewLine()
+        {
+            WordWrapper.Wrap("a ab", 2).ShouldBe("a\nab");
+        }
+
+        [Fact]
+        public void SpaceAfterMaxLengthIsReplacedByNewLine()
+        {
+            WordWrapper.Wrap("aa b", 1).ShouldBe("aa\nb");
+        }
+
+        [Fact]
+        public void OnlySpacesArroundMaxLengthAreReplacedByNewLine()
+        {
+            WordWrapper.Wrap("a  ab", 2).ShouldBe("a \nab");
+        }
+
+        [Fact]
+        public void AllSpacesAtEachMaxLengthAreReplacesByNewLine()
+        {
+            WordWrapper.Wrap("a a b", 1).ShouldBe("a\na\nb");
+        }
+
+        [Fact]
+        public void StringsLargerMaxLengthAreSeparatedByNewLineAtMaxLength()
+        {
+            WordWrapper.Wrap("aab", 2).ShouldBe("aa\nb");
+        }
+
+        [Fact]
+        public void ComplexeExample()
+        {
+            WordWrapper.Wrap("Heute ist ein schöner Tag, weil ich meinen Job so mag.", 10).ShouldBe("Heute ist\nein schöner\nTag, weil\nich meinen\nJob so mag.");
+        }
+    }
+}
---
+++ WordWrap/WordWrap/WordWrapper.cs
@@ -0,0 +1,47 @@
+﻿namespace WordWrap
+{
+    using System.Collections.Generic;
+
+    public static class WordWrapper
+    {
+        public static string Wrap(string text, int maxLength)
+        {
+            if (text != null)
+            {
+                var result = new List<char>(text.ToCharArray());
+                for (var position = maxLength; position <= text.Length; position += maxLength +1)
+                {
+                    GetValue(ref position, result);
+                }
+
+                return new string(result.ToArray());
+            }
+
+            return null;
+        }
+
+        private static void GetValue(ref int position, List<char> result)
+        {
+            if (position < result.Count)
+            {
+                if (result[position] == ' ')
+                {
+                    result[position] = '\n';
+                }
+                else if (result.Count >= position - 1 && position > 0 && result[position - 1] == ' ')
+                {
+                    result[position - 1] = '\n';
+                }
+                else if (result.Count > position + 1 && result[position + 1] == ' ')
+                {
+                    result[position + 1] = '\n';
+                    position++;
+                }
+                else
+                {
+                    result.Insert(position, '\n');
+                }
+            }
+        }
+    }
+}
