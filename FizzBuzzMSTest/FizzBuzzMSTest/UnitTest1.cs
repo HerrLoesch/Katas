3a452c3977c08a06e769245a27db211d3d8a868a
Hendrik Lösch <loeschhendrik@googlemail.com>
2016-04-27 11:06:01 +0200

    Simple Solution

--- FizzBuzzMSTest/FizzBuzzMSTest/UnitTest1.cs
+++ FizzBuzzMSTest/FizzBuzzMSTest/UnitTest1.cs
@@ -4,8 +4,110 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace FizzBuzzMSTest
 {
     [TestClass]
-    public class UnitTest1
+    public class FizzBuzzConverterTests
     {
-    
+        private FizzBuzzConverter sut;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            this.sut = new FizzBuzzConverter();
+        }
+
+        [TestMethod]
+        public void Number_1_ResultsIn_1()
+        {
+            var result = this.sut.Convert(1);
+
+            Assert.AreEqual(1, result);
+        }
+
+        [TestMethod]
+        public void Number_2_ResultsIn_2()
+        {
+            var result = this.sut.Convert(2);
+
+            Assert.AreEqual(2, result);
+        }
+
+        [TestMethod]
+        public void Number_3_ResultsIn_Fizz()
+        {
+            var result = this.sut.Convert(3);
+
+            Assert.AreEqual("Fizz", result);
+        }
+
+        [TestMethod]
+        public void Number_4_ResultsIn_4()
+        {
+            var result = this.sut.Convert(4);
+
+            Assert.AreEqual(4, result);
+        }
+
+        [TestMethod]
+        public void Number_5_ResultsIn_Buzz()
+        {
+            var result = this.sut.Convert(5);
+
+            Assert.AreEqual("Buzz", result);
+        }
+
+        [TestMethod]
+        public void Number_6_ResultsIn_Fizz()
+        {
+            var result = this.sut.Convert(6);
+
+            Assert.AreEqual("Fizz", result);
+        }
+
+        [TestMethod]
+        public void Number_10_ResultsIn_Buzz()
+        {
+            var result = this.sut.Convert(10);
+
+            Assert.AreEqual("Buzz", result);
+        }
+
+        [TestMethod]
+        public void Number_15_ResultsIn_FizzBuzz()
+        {
+            var result = this.sut.Convert(15);
+
+            Assert.AreEqual("FizzBuzz", result);
+        }
+
+        [TestMethod]
+        public void Number_30_ResultsIn_FizzBuzz()
+        {
+            var result = this.sut.Convert(30);
+
+            Assert.AreEqual("FizzBuzz", result);
+        }
+    }
+
+    public class FizzBuzzConverter
+    {
+        public object Convert(int number)
+        {
+            object result = null;
+            if (number % 3 == 0)
+            {
+                result += "Fizz";
+            }
+
+            if (number % 5 == 0)
+            {
+                result += "Buzz";
+            }
+
+            if (result != null)
+            {
+                return result;
+            }
+
+            return number;
+        }
     }
 }
